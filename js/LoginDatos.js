65461172b7d63cae3500be78a9b10e0da9e0b8ae
Allenrovas <2909080001301@ingenieria.usac.edu.gt>
2022-06-08 04:13:32 -0600

    Segundo commit

---
+++ js/LoginDatos.js
@@ -0,0 +1,9 @@
+document.getElementById("btn_login").onclick=function(){
+    alert("Login");
+    var usuario, password;
+    usuario = document.getElementById("usuarioLogin").value;
+    password = document.getElementById("usuarioContra").value;
+    
+    console.log(usuario);
+
+}
